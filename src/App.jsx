62edf31921064a429410a4af9f7747ca8a71ffbf
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-07-20 17:21:57 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import "./App.css";
+// import Header from "./Header.jsx";
+
+
+function App(){
+    return(
+        <div className="grid-container">
+            {/* <Header className="grid-header"/> */}
+            <div className="grid-header header">123</div>
+            <div className="grid-main main">
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+                <div>main</div>
+
+            </div>
+            <div className="grid-footer footer">FOOTER</div>
+        </div>
+    );
+}
+
+export default App;
