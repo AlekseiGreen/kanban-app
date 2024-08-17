ef5ead00735fa38d5e819c5a6790a0bf23041040
AlekseiGreen <spacegoogls@gmail.com>
2024-08-18 00:57:45 +0300

    commit Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,9 @@ function Header(){
     return(
         <div className="grid-header header">
             <div className="header-logo">Awesome Kanban Board</div>
-            
+            <div className="user-avatar">
+                <img src="/img/user.svg" alt="user-avatar" className="user__img" />
+            </div>
         </div>
     );
 }
