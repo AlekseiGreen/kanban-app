fce57d3318e50c11bde3c2eb54ff16e6719a7bd2
AlekseiGreen <spacegoogls@gmail.com>
2024-08-17 22:17:10 +0300

    comm new

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
-import "./Header.css"
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import "./Header.css"
 
 
 function Header(){
     return(
         <div className="grid-header header">
-            1234
+            <div className="header-logo">Awesome Kanban Board</div>
+            
         </div>
     );
 }
