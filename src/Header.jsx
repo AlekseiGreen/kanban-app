7a67d51977caa5f7826a1c7ca377f19c58be5412
AlekseiGreen <spacegoogls@gmail.com>
2024-08-18 09:36:12 +0300

    Add new Head

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 // import React, { useState } from 'react'
 // import { Link } from 'react-router-dom'
 // import "./Header.css"
+import User from "./img/user-avatar.svg";
 
 
 function Header(){
@@ -8,7 +9,7 @@ function Header(){
         <div className="grid-header header">
             <div className="header-logo">Awesome Kanban Board</div>
             <div className="user-avatar">
-                <img src="/img/user.svg" alt="user-avatar" className="user__img" />
+                <img src={User} alt="user-avatar" />
             </div>
         </div>
     );
