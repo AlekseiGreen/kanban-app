349d59e3f3c774ae4e09d90dbdda39c917e518b4
AlekseiGreen <spacegoogls@gmail.com>
2024-08-30 21:14:58 +0300

    Add new code in Block

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,26 +1,35 @@
+import {useRef} from 'react';
 import "./Block.css";
 
+let textData;
+
+
 function Block(props){
-    let textData;
+    const fileInputRef = useRef();
+    console.log(fileInputRef);
+    
     const handleEnter = (event)=>{
         console.log("BLOCK=",event.target.value);
         textData = event.target.value;
     }
-    function onSetData(){
-        props.onEnter(textData);
-    }
 
+    function onSendData(in_textData){
+        console.log("Ref=", fileInputRef.current.value);
+        if(in_textData === undefined || in_textData === ""){
+            console.log("нет данных");
+        } else{
+            console.log("Send data=", in_textData);
+            props.onEnter(in_textData);
+            fileInputRef.current.value ="";
+        }
+    }
 
     return(
         <div className="block">
             <div className="block-title">{props.array.title}</div>
             {props.array.issues.map(element=> <div className="block-content">{element.name}</div>)}
-            {/* <div className="block-input"><input type="text" onChange={handleEnter}/></div>
-            <div className="block-add"><div className="block-add-text" onClick={props.onFunction}>+ Add card</div></div> */}
-            <div className="block-input"><input type="text" onChange={handleEnter}/></div>
-            <div className="block-add"><div className="block-add-text" onClick={onSetData}>+ Add card</div></div>
-            
-
+            {true && <div className="block-input"><input type="text" ref={fileInputRef} onChange={handleEnter}/></div>}
+            <div className="block-add"><div className="block-add-text" onClick={()=>onSendData(textData)}>+ Add card</div></div>
         </div>
     );
 }
