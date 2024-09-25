788372b8f15535b2936de72079e0ff60bd735c0e
AlekseiGreen <spacegoogls@gmail.com>
2024-09-25 22:41:25 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import "./App.css";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
 import Footer from "./Footer.jsx";
-import Detail from "./Detail";
+import Detail from "./Detail.jsx";
+import Task from "./components/Task.jsx";
 
 
 function App(){
+
+    let rR = <Route path="/task" element={<Task/>}/>;
 
     return(
         <Router>
@@ -14,7 +17,7 @@ function App(){
             
                 <Routes>
                     <Route path="/" element={<Main/>}/>
-                    {/* implement a route cycle */}
+                    {rR}
                     <Route path="/detail" element={<Detail/>}/>
                     <Route path="*" element={<h1>Page not found</h1>}/>
                 </Routes>
